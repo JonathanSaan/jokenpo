e59e4ea3dd58f0a781d5bfe000a2bc6dc91d5b6e
Jonathan <52434574+JonathanSaan@users.noreply.github.com>
2022-02-01 19:39:09 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+let pedra = document.querySelector('#pedra')
+let papel = document.querySelector('#papel')
+let tesoura = document.querySelector('#tesoura')
+function Computer(){
+  
+}
+
+pedra.addEventListener('click', function() {
+ console.log('pedra')
+});
